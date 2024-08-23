73f88c425e386b9b59eef28301db4111360fcc68
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-23 17:32:50 -0300

    Adding exception types

---
+++ Model/AcceptanceCriteriaNotValidException.cs
@@ -0,0 +1,6 @@
+﻿namespace TestManager.Exceptions;
+
+public class AcceptanceCriteriaNotValidException : Exception
+{
+    public AcceptanceCriteriaNotValidException(string useCase, string acceptanceCriteria) : base($"The use case {useCase} was not passed the acceptance criteria {acceptanceCriteria}") { }
+}

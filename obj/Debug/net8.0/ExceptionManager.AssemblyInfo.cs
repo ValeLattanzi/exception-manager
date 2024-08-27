90c7affc902ca2efe8b534588348d06bf7a7f8dc
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-27 10:21:43 -0300

    publish

--- obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc381bcf81da8adcd970447674918a6accbf247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a642b6b6c71bab1a5b8b6606e70de11e813aa2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

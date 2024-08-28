3119f3d405154a67e6b8f88fbf0f83d261c0eff2
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-28 16:18:01 -0300

    fetch to date

--- obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a642b6b6c71bab1a5b8b6606e70de11e813aa2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e4bf484f095318eab4c292bd30e505e0827af7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

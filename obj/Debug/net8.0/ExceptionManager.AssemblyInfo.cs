73b0d229a29e11266ab37047b22ac61f6911f8b1
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-08 16:15:13 -0300

    feat: create client

--- obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2654563ba2e71c08bff3d985d833b8f3c0c4df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553e2ed668bf876318d20d74297f3018c4ba8768")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

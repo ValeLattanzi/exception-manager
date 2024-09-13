f632aa1df4aa49a36ca07379795d0c07b90c3a5e
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-13 11:40:27 -0300

    architect

--- obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExceptionManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553e2ed668bf876318d20d74297f3018c4ba8768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b0d229a29e11266ab37047b22ac61f6911f8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExceptionManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
